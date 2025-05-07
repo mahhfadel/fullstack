7b8c55d232f02c1a22c91a1b8f8cec03fc9b112d
Maria Clara Fadel <mariaclarapereira@alunos.utfpr.edu.br>
2025-05-06 22:14:29 -0300

    refactor style

--- fullstack-lucas-e-maria/src/components/SearchCrypto.jsx
+++ fullstack-lucas-e-maria/src/components/SearchCrypto.jsx
@@ -1,47 +1,73 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { Box, Button, Card, CardContent, Container, TextField, Typography } from '@mui/material';
 
 const SearchCrypto = () => {
   const inputRef = useRef();
+  const [hasError, setHasError] = useState(false);  
 
   const handleSearch = () => {
     const query = inputRef.current.value.trim();
     if (!query) {
-      alert('Por favor, digite o nome de uma criptomoeda.');
+      setHasError(true); 
       return;
     }
 
+    setHasError(false); 
+    alert('Pesquisa realizada!');
     console.log('Criptomoeda digitada:', query);
     // Aqui depois vamos fazer a chamada para a API da CoinGecko com esse valor
   };
 
   return (
-    <Box sx={{ minHeight: '100vh', backgroundColor: '#fdec6f', display: 'flex', alignItems: 'center' }}>
+    <Box sx={{ minHeight: '100vh', background: 'linear-gradient(to bottom, #0B0C10, #1F2235)', display: 'flex', alignItems: 'center' }}>
       <Container maxWidth="sm">
-        <Card elevation={4}>
-          <CardContent>
+        <Typography variant="h2" component="h1" align="center" color='#E3E3FF' marginBottom='32px'>
+          Cripto Coins Master
+        </Typography>
+
+        <Card sx={{boxShadow: '0 0 12px #9D00FF'}}>
+          <CardContent sx={{backgroundColor: '#1A1F2B'}}>
             <Box sx={{ display: 'flex', flexDirection: 'column', gap: 3 }}>
-              <Typography variant="h4" component="h1" align="center">
-                Buscar Criptomoeda
-              </Typography>
               <TextField
-                label="Digite o nome (ex: bitcoin)"
+                label="Buscar criptmoedas"
                 variant="outlined"
                 inputRef={inputRef}
                 fullWidth
+                error={hasError} 
+                helperText={hasError ? "Este campo é obrigatório" : ""}  
+                sx={{
+                  input: { color: '#E3E3FF' },
+                  label: {
+                    color: '#E3E3FF',
+                    '&.Mui-focused': {
+                      color: '#E3E3FF', 
+                    },
+                  },
+                  '& .MuiOutlinedInput-root': {
+                    '& fieldset': {
+                      borderColor: '#E3E3FF',
+                    },
+                    '&:hover fieldset': {
+                      borderColor: '#E3E3FF',
+                    },
+                    '&.Mui-focused fieldset': {
+                      borderColor: '#E3E3FF',
+                    },
+                  },
+                }}
               />
               <Button
                 variant="contained"
-                sx={{ //sx permite que estilize um componente do MUI usando uma sintaxe de objeto JavaScript
-                    backgroundColor: '#1f0b0c',
-                    '&:hover': {
-                    backgroundColor: '#361f2d',
-                    },
+                sx={{ 
+                  backgroundColor: '#9D00FF',
+                  '&:hover': {
+                    backgroundColor: '#7F00FF',
+                  },
                 }}
                 onClick={handleSearch}
-                >
+              >
                 Buscar
-                </Button>
+              </Button>
             </Box>
           </CardContent>
         </Card>
